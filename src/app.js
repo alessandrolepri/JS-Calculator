8f1f8b2bbfa99bd6521fdf73a847b91fab91a984
Alessandro Lepri <alex.lepri84@gmail.com>
2020-02-23 14:04:41 +0000

    each click into historyarray

--- src/app.js
+++ src/app.js
@@ -1,1 +1,43 @@
 import './sass/style.sass'
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  const historyarray = []
+
+  document.querySelectorAll('button').forEach((button) => button.addEventListener('click', ({target}) => calculate(target.getAttribute('value'))))
+
+  function calculate(keyitem) {
+    historyarray.push(keyitem)
+    switch(keyitem) {
+      case 'clear':
+        break
+      case '/':
+      case '*':
+      case '+':
+      case '-':
+        break
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        break
+      case '.':
+        break
+      case '=':
+        break
+    }
+    console.log(historyarray)
+  }
+
+
+
+
+
+
+})
