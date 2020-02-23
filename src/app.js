fdaec7c7cbb465ffa5feae3fd6004659c0955709
Alessandro Lepri <alex.lepri84@gmail.com>
2020-02-23 14:39:57 +0000

    Getting number plus decimal into history array, AC button is clearing the array

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import './sass/style.sass'
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  const historyarray = []
+  let historyarray = []
+  let mainMath = '0'
+  let history = '0'
+  let finalset = ''
+  let equalPressed = false
 
   document.querySelectorAll('button').forEach((button) => button.addEventListener('click', ({target}) => calculate(target.getAttribute('value'))))
 
@@ -10,11 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
     historyarray.push(keyitem)
     switch(keyitem) {
       case 'clear':
+        clearScreen()
         break
       case '/':
       case '*':
       case '+':
       case '-':
+        addOperator(keyitem)
         break
       case '0':
       case '1':
@@ -26,18 +32,78 @@ document.addEventListener('DOMContentLoaded', function() {
       case '7':
       case '8':
       case '9':
+        addNumber(keyitem)  // adding only numbers
         break
       case '.':
+        addDecimal(keyitem)
         break
       case '=':
         break
     }
+    // console.log(historyarray)
+  }
+
+  function addNumber(keyitem) {
+    if (mainMath === '0'){
+      mainMath = keyitem
+      finalset = mainMath
+      return
+    }
+    mainMath+=keyitem
+    finalset = mainMath
+    // console.log(finalset)
+    console.log(mainMath)
+  }
+
+  // getting every values into historyarray
+
+  function addOperator(keyitem){
+    if(['/', 'x', '+', '-', '='].indexOf(historyarray[0]) > -1)   {
+      historyarray = []
+      return
+    }
+    if (equalPressed === true){
+      history = ''
+      historyarray = []
+      equalPressed = false
+    }
+    mainMath+=keyitem
+
+    if(history === '0'){
+      history = mainMath
+    } else {
+      history += mainMath
+    }
+    mainMath = '0'
     console.log(historyarray)
   }
 
 
+  // getting the history array back to original value of 0 after click on AC
+  const clearScreen = () => {
+    mainMath = '0'
+    history = '0'
+    historyarray = []
+  }
+
+
+  // Allowing decimal to go into historyarray when press '.' value
+  function addDecimal(keyitem) {
+    if (mainMath.indexOf(keyitem) === -1){
+      if(mainMath === '0') {
+        mainMath = '0' + keyitem
+        return
+      }
+    } else {
+      return
+    }
+    mainMath+=keyitem
+  }
+
+  // console.log(historyarray)
 
 
 
 
-})
+
+}) //DOMContentLoaded ends
